686a3564b7fbba2f7224c3f7ef3ff1a07e78f19f
sabarish727 <93857728+sabarish727@users.noreply.github.com>
2021-11-28 13:49:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+function compute()
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
